84c8575850c4046a96de82bf112783591520e5a1
Carlos-Gonzalez-Ruiz <carlosgonzrz@gmail.com>
2023-04-26 07:06:42 +0200

    Fix server 6

--- src/main/java/com/carlosgonzalezruiz/aliensupremacyserver/AlienSupremacyServerApplication.java
+++ src/main/java/com/carlosgonzalezruiz/aliensupremacyserver/AlienSupremacyServerApplication.java
@@ -1,8 +1,29 @@
 package com.carlosgonzalezruiz.aliensupremacyserver;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
-import com.carlosgonzalezruiz.aliensupremacyserver.game.server.ThreadServer;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+
+import org.java_websocket.server.DefaultSSLWebSocketServerFactory;
 
 /**
  * Alien Supremacy - Proyecto Fin de Ciclo
@@ -21,11 +42,16 @@ public class AlienSupremacyServerApplication {
 	 * 
 	 * @param args
 	 * @throws IOException 
+	 * @throws KeyStoreException 
+	 * @throws CertificateException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws UnrecoverableKeyException 
+	 * @throws KeyManagementException 
 	 */
-	public static void main(String[] args) throws IOException {
-		log.info("Application started...");
+	//public static void main(String[] args) throws IOException, KeyStoreException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException, KeyManagementException {
+		//log.info("Application started...");
 		
-		// Iniciar servidor.
+		/*// Iniciar servidor.
 		ThreadServer threadServer = new ThreadServer();
 		threadServer.start();
 
@@ -35,7 +61,89 @@ public class AlienSupremacyServerApplication {
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 			log.error("Failed to join the thread: {}", e.getMessage());
-		}
-	}
+		}*/
+	//}
+	
+	public static void main(String[] args) throws Exception {
+	    ChatServer chatserver = new ChatServer(8887);
+
+	    SSLContext context = getContext();
+	    if (context != null) {
+	      chatserver.setWebSocketFactory(new DefaultSSLWebSocketServerFactory(getContext()));
+	    }
+	    chatserver.setConnectionLostTimeout(30);
+	    chatserver.start();
+
+	  }
+
+	  private static SSLContext getContext() {
+	    SSLContext context;
+	    String password = "CHANGEIT";
+	    String pathname = "pem";
+	    try {
+	      context = SSLContext.getInstance("TLS");
+
+	      byte[] certBytes = parseDERFromPEM(getBytes(new File(pathname + File.separator + "cert.pem")),
+	          "-----BEGIN CERTIFICATE-----", "-----END CERTIFICATE-----");
+	      byte[] keyBytes = parseDERFromPEM(
+	          getBytes(new File(pathname + File.separator + "privkey.pem")),
+	          "-----BEGIN PRIVATE KEY-----", "-----END PRIVATE KEY-----");
+
+	      X509Certificate cert = generateCertificateFromDER(certBytes);
+	      RSAPrivateKey key = generatePrivateKeyFromDER(keyBytes);
+
+	      KeyStore keystore = KeyStore.getInstance("JKS");
+	      keystore.load(null);
+	      keystore.setCertificateEntry("cert-alias", cert);
+	      keystore.setKeyEntry("key-alias", key, password.toCharArray(), new Certificate[]{cert});
+
+	      KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+	      kmf.init(keystore, password.toCharArray());
+
+	      KeyManager[] km = kmf.getKeyManagers();
+
+	      context.init(km, null, null);
+	    } catch (Exception e) {
+	      context = null;
+	    }
+	    return context;
+	  }
+
+	  private static byte[] parseDERFromPEM(byte[] pem, String beginDelimiter, String endDelimiter) {
+	    String data = new String(pem);
+	    String[] tokens = data.split(beginDelimiter);
+	    tokens = tokens[1].split(endDelimiter);
+	    return DatatypeConverter.parseBase64Binary(tokens[0]);
+	  }
+
+	  private static RSAPrivateKey generatePrivateKeyFromDER(byte[] keyBytes)
+	      throws InvalidKeySpecException, NoSuchAlgorithmException {
+	    PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+
+	    KeyFactory factory = KeyFactory.getInstance("RSA");
+
+	    return (RSAPrivateKey) factory.generatePrivate(spec);
+	  }
+
+	  private static X509Certificate generateCertificateFromDER(byte[] certBytes)
+	      throws CertificateException {
+	    CertificateFactory factory = CertificateFactory.getInstance("X.509");
+
+	    return (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(certBytes));
+	  }
+
+	  private static byte[] getBytes(File file) {
+	    byte[] bytesArray = new byte[(int) file.length()];
+
+	    FileInputStream fis = null;
+	    try {
+	      fis = new FileInputStream(file);
+	      fis.read(bytesArray); //read file into bytes[]
+	      fis.close();
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+	    return bytesArray;
+	  }
 
 }
