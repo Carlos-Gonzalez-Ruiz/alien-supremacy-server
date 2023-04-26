c4429403affdafc04011404e8ea410fc9b03d3c3
Carlos-Gonzalez-Ruiz <carlosgonzrz@gmail.com>
2023-04-26 03:48:50 +0200

    Fix jar 3

--- src/main/java/com/carlosgonzalezruiz/aliensupremacyserver/constant/GameConstants.java
+++ src/main/java/com/carlosgonzalezruiz/aliensupremacyserver/constant/GameConstants.java
@@ -11,7 +11,7 @@ package com.carlosgonzalezruiz.aliensupremacyserver.constant;
 public final class GameConstants {
 
 	/** Puerto del que se instanciará el servidor de juego. */
-	public static final int SERVER_PORT = 433/*42000*/;
+	public static final int SERVER_PORT = 42000;
 	/** Tiempo en milisegundos que esperan los hilos cliente. */
 	public static final int SERVER_TICK_RATE = 16*0+100;
 
